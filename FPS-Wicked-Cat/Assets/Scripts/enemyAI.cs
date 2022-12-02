6e453cab0d23f519236ce4ff61c33f631e43d12c
Ajcardoso <102987685+ajcardoso@users.noreply.github.com>
2022-12-02 18:13:30 -0500

    IDamage Script created

--- FPS-Wicked-Cat/Assets/Scripts/enemyAI.cs
+++ FPS-Wicked-Cat/Assets/Scripts/enemyAI.cs
@@ -5,24 +5,30 @@ using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
-    [Header("-- Components --")]
+    [Header("-- Enemy Components --")]
     [SerializeField] int HP;
-    [SerializeField] Renderer model;
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] int enemySpeed;
+    [SerializeField] Renderer model;        // enemy model
+    [SerializeField] NavMeshAgent agent;    // can walk on surface
 
-
+    [Header("Move Points")]
+    public Transform[] movePoints; // this will be empty objects 
+                                   // just like the respawn object
+    public int HPOrig;
     public Transform player;
+    bool isShooting, playerInRange;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        HPOrig = HP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        AiMovement();
+        
     }
 
     IEnumerator dmgFlash()
@@ -32,8 +38,30 @@ public class enemyAI : MonoBehaviour
         model.material.color = Color.white;
     }
 
+
+
     void AiMovement()
     {
         agent.SetDestination(player.position);
+
+    }
+
+    public void Patrol()
+    {
+            
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+
+        AiMovement(); // after enemy takes damage being to move 
+        
+        StartCoroutine(dmgFlash());
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
