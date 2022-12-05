39d4bf21bc9f801d885b4c674acc5067418fac4b
Garrett Wilkinson <garrettwilkinson7@gmail.com>
2022-12-05 02:56:54 -0600

    Created the bullet script

---
+++ FPS-Wicked-Cat/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int despawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, despawnTimer);
+    }
+}
