f21352ecef9e8dd9b216403105bb132dd043c437
Ajcardoso <102987685+ajcardoso@users.noreply.github.com>
2022-12-08 12:39:10 -0500

    playerBullet script and enemyBullet script implemented

--- FPS-Wicked-Cat/Assets/Scripts/enemyBullet.cs
+++ FPS-Wicked-Cat/Assets/Scripts/enemyBullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyBullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
 
