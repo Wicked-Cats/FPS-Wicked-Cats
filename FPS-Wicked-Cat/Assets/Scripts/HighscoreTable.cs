2b60322e1fc305de77aa822c8667d5e7609d4a1e
Jacob Taylor <JATaylor2@student.fullsail.edu>
2023-01-12 19:02:49 -0700

    evening updates

--- FPS-Wicked-Cat/Assets/Scripts/HighscoreTable.cs
+++ FPS-Wicked-Cat/Assets/Scripts/HighscoreTable.cs
@@ -1,29 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class HighscoreTable : MonoBehaviour
-{
-   
-    [SerializeField] Transform entryContainer;
-    [SerializeField] Transform entryTemplate;
+//public class HighscoreTable : MonoBehaviour
+//{
 
-    private void Awake()
-    {
+//    [SerializeField] Transform entryContainer;
+//    [SerializeField] Transform entryTemplate;
+//    //[SerializeField] Transform entryRank;
+//    //[SerializeField] Transform entryScore;
+//    //[SerializeField] Transform entryTimeSurv;
+//    //[SerializeField] Transform entryEnemiesKilled;
+//    //[SerializeField] Transform entryUserdeathCount;
+//    //[SerializeField] Transform entryUserName;
 
-        entryContainer = transform.Find("EntryContainer");
-        entryTemplate = entryContainer.Find("EntryTemp");
+//    private void Awake()
+//    {
 
-        entryTemplate.gameObject.SetActive(false);
+//        entryContainer = transform.Find("EntryContainer");
+//        entryTemplate = entryContainer.Find("EntryTemp");
 
-        float templateHeight = 20f;
+//        //entryTemplate.gameObject.SetActive(false);
 
-        for (int i = 0; i < 10; i++)
-        {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRect = entryTransform.GetComponent<RectTransform>();
-            entryRect.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
-        }
-    }
-}
+//        //float templateHeight = 30f;
+
+//        //for (int i = 0; i < 10; i++)
+//        //{
+//        //    Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+//        //    RectTransform entryRect = entryTransform.GetComponent<RectTransform>();
+//        //    entryRect.anchoredPosition = new Vector2(0, -templateHeight * i);
+//        //    entryTransform.gameObject.SetActive(true);
+
+//            //int rank = i + 1;
+
+//            //string rankString;
+
+//            //switch (rank)
+//            //{
+//            //    default:
+//            //        rankString = rank + "Th"; break;
+
+//            //    case 1: rankString = "1ST"; break;
+//            //    case 2: rankString = "2ND"; break;
+//            //    case 3: rankString = "3RD"; break;
+
+//            //}
+//            //entryTransform.Find("_Pos").GetComponent<TextMeshProUGUI>().text = rankString;
+
+//            //int score = Random.Range(0, 100000);
+//            //entryTransform.Find("_ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+//            //string time = "12:00";
+//            //entryTransform.Find("_TimeSurvived").GetComponent<TextMeshProUGUI>().text = time;
+//            //int killCou = Random.Range(0, 100);
+//            //entryTransform.Find("_EnemyKills").GetComponent<TextMeshProUGUI>().text = killCou.ToString();
+//            //int userDeath = Random.Range(0, 100);
+//            //entryTransform.Find("_UserDeathCount").GetComponent<TextMeshProUGUI>().text = userDeath.ToString();
+//            //string name = "JJJJ";
+//            //entryTransform.Find("_UserName").GetComponent<TextMeshProUGUI>().text = name;
+
+
+
+
+//        }
+//    }
+//}
