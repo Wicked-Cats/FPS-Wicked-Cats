9db30b7548366c07602fdf062ef51f2ddbbb2470
Jacob Lacey <jacoblacey34@gmail.com>
2023-01-17 12:50:49 -0600

    highscore table base functions

--- FPS-Wicked-Cat/Assets/Scripts/HighScoreTable.cs
+++ FPS-Wicked-Cat/Assets/Scripts/HighScoreTable.cs
@@ -12,20 +12,24 @@ public class HighScoreTable : MonoBehaviour
     private Transform entryTemplate;
     private HighscoreEntryData[] highscoreArray;
     private List<Transform> highscoreEntryTransformList;
+    [SerializeField] TextMeshProUGUI[] names;
+    [SerializeField] TextMeshProUGUI[] scores;
+    [SerializeField] TextMeshProUGUI[] enemiesKilleds;
+    [SerializeField] TextMeshProUGUI[] timeSurviveds;
 
-    public TextMeshProUGUI userName;
-    public TextMeshProUGUI score;
-    public TextMeshProUGUI enemyKillCount;
-    public TextMeshProUGUI timeSurvived;
-
+    public TextMeshProUGUI userName;                                                                                                               
+    public TextMeshProUGUI score;                                                                                                                  
+    public TextMeshProUGUI enemyKillCount;                                                                                                         
+    public TextMeshProUGUI timeSurvived;                                                                                                           
+                                                                                                                                                   
     private void Awake()
     //public void HighScoreTableEntry()
     {
 
-        entryContainer = transform.Find("EntryContainer");
-        entryTemplate = entryContainer.Find("EntryTemp");
+        //entryContainer = transform.Find("EntryContainer");
+        //entryTemplate = entryContainer.Find("EntryTemp");
 
-        entryContainer.gameObject.SetActive(false);
+        //entryContainer.gameObject.SetActive(false);
 
         
 
@@ -36,7 +40,7 @@ public class HighScoreTable : MonoBehaviour
             new HighscoreEntryData("RAT",  43223,  12,  "12:00") ,
             new HighscoreEntryData("JAT", 3223,  12, "22:00"), 
             new HighscoreEntryData( "DJT",  44432,  12,  "14:00" ) ,
-            new HighscoreEntryData(  "NAT",  44433, 12,  "3:00" ) ,
+            new HighscoreEntryData(  "NAT",  44433, 123,  "3:00" ) ,
             new HighscoreEntryData("SBC", 444, 12, "5:00") ,
             new HighscoreEntryData("AMT", 4423, 12, "8:00"), 
             new HighscoreEntryData("BWT", 423, 12, "9:00") ,
@@ -52,9 +56,19 @@ public class HighScoreTable : MonoBehaviour
     private void Start()
     {
 
-        highscoreEntryTransformList = new List<Transform>();
 
-        //List shorting by score
+
+
+
+
+
+
+
+
+
+        //    highscoreEntryTransformList = new List<Transform>();
+
+        //    //List shorting by score
         for (int i = 0; i < highscoreArray.Length; i++)
         {
             for (int j = i + 1; j < highscoreArray.Length; j++)
@@ -69,28 +83,34 @@ public class HighScoreTable : MonoBehaviour
             }
         }
 
-
-
-        foreach (HighscoreEntryData highscoreEntry in highscoreArray)
+        for(int i = 0; i < highscoreArray.Length; i++)
         {
-            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+            names[i].text = highscoreArray[i].name;
+            scores[i].text = highscoreArray[i].score.ToString();
+            enemiesKilleds[i].text = highscoreArray[i].enemykillCount.ToString();
+            timeSurviveds[i].text = highscoreArray[i].timeSurvived.ToString();
         }
-    }
 
-    private void CreateHighscoreEntryTransform(HighscoreEntryData highscoreEntry, Transform container, List<Transform> transformlist)
-    {
-        Transform entryTransform = Instantiate(entryTemplate, container);
-        RectTransform entry = entryTransform.GetComponent<RectTransform>();
-        entry.anchoredPosition = new Vector2(0, transformlist.Count);
-        entryContainer.gameObject.SetActive(true);
+        //    foreach (HighscoreEntryData highscoreEntry in highscoreArray)
+        //    {
+        //        CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        //    }
+        //}
 
-        // sets text to variables
-        userName.text = highscoreEntry.name;
-        score.text = highscoreEntry.score.ToString();
-        enemyKillCount.text = highscoreEntry.score.ToString();
-        timeSurvived.text = highscoreEntry.score.ToString();
+        //private void CreateHighscoreEntryTransform(HighscoreEntryData highscoreEntry, Transform container, List<Transform> transformlist)
+        //{
+        //    Transform entryTransform = Instantiate(entryTemplate, container);
+        //    RectTransform entry = entryTransform.GetComponent<RectTransform>();
+        //    entry.anchoredPosition = new Vector2(0, transformlist.Count);
+        //    entryContainer.gameObject.SetActive(true);
 
-        transformlist.Add(entryTransform);
+        //    // sets text to variables
+        //    userName.text = highscoreEntry.name;
+        //    score.text = highscoreEntry.score.ToString();
+        //    enemyKillCount.text = highscoreEntry.score.ToString();
+        //    timeSurvived.text = highscoreEntry.score.ToString();
+
+        //    transformlist.Add(entryTransform);
     }
 
     private void AddHighscoreEntry(string name, int score,int enemykillCount , string timeSurvived)
