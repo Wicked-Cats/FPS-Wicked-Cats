0f20c56fc248c317c85835b9666cc7ecda487fa3
Jacob Lacey <jacoblacey34@gmail.com>
2023-01-23 17:14:22 -0600

    tweaks

---
+++ FPS-Wicked-Cat/Assets/Scripts/lightONOFF.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightONOFF : MonoBehaviour
+{
+    private Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((gameManager.instance.player.transform.position - this.transform.position).magnitude > 50)
+        {
+            light.enabled = false;
+        }
+        else
+        {
+            light.enabled = true;
+        }
+    }
+}
