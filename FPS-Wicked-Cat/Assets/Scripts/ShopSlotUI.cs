6ee5c6558c9dd6ad34b2b2575b5e26e41dbf991f
Garrett Wilkinson <garrettwilkinson7@gmail.com>
2023-01-22 18:49:58 -0600

    changes with shop UI

--- FPS-Wicked-Cat/Assets/Scripts/ShopSlotUI.cs
+++ FPS-Wicked-Cat/Assets/Scripts/ShopSlotUI.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class ShopSlotUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image _itemSprite;
+    [SerializeField] TextMeshProUGUI _itemName;
+    [SerializeField] TextMeshProUGUI _itemCost;
+
+    [SerializeField] private Button _addItemToCartButton;
+    [SerializeField] private Button _removeItemFromCartButton;
+
+    public ShopKeeperDisplay ParentDisplay { get; private set; }
+
+    private void Awake()
     {
-        
+        _itemSprite.sprite = null;
+        _itemSprite.preserveAspect = true;
+        _itemSprite.color = Color.clear;
+        _itemName.text = "";
+        _itemCost.text = "";
+
+        _addItemToCartButton?.onClick.AddListener(AddItemToCart);
+        _removeItemFromCartButton?.onClick.AddListener(RemoveItemFromCart);
+
+        ParentDisplay.transform.parent.GetComponentInParent<ShopKeeperDisplay>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RemoveItemFromCart()
     {
-        
+        Debug.Log("Removing item from cart");
+    }
+
+    private void AddItemToCart()
+    {
+        Debug.Log("Adding item to cart");
+    }
+
+
+    public void SetSprite(Image sprite)
+    {
+        _itemSprite = sprite;
+    }
+
+    public void SetItemName(string itemName)
+    {
+        _itemName.SetText(itemName);
+    }
+
+    public void SetItemCost(string itemCost)
+    {
+        _itemCost.SetText(itemCost);
     }
 }
